bcdc42ded55bf0a154b5da935dd7cc700012688d
André Company Gianesi <andrecompanygianesi@Andres-MacBook-Air.local>
2024-12-03 00:48:30 -0500

    created virtual

--- src/models/User.ts
+++ src/models/User.ts
@@ -29,6 +29,10 @@ const userSchema = new Schema(
     },
 );
 
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
 const User = model('User', userSchema);
 
 export default User;
